14c0a9206c70b76497d3b07289fb982b6267658e
Hayden Barnes <120114860+haydencbarnes@users.noreply.github.com>
2023-10-03 20:58:35 +0000

    feature: added full asAppliedData flow to py & js

--- code_samples/python/downloadAsAppliedData.py
+++ code_samples/python/downloadAsAppliedData.py
@@ -26,11 +26,28 @@ def main():
         activities_response = requests.get(f'https://cloud.api.trimble.com/Trimble-Ag-Software/externalApi/3.0/tasks/{org_id}/equipmentactivities', headers=headers)
         activities_data = activities_response.json()
         print(json.dumps(activities_data, indent=2))
-        # Process the data received in the response
+        
+        # EDIT HERE: Choose the desired activity by its index in the array.
+        activity_index = 0
+        equipment_activity_id = activities_data['result'][activity_index]['id']
+
+        # Step 2: GET - Get the list of Layers for an Equipment Activity ID
+        layers_response = requests.get(f'https://cloud.api.trimble.com/Trimble-Ag-Software/externalApi/3.0/tasks/{org_id}/layers?equipmentActivityId={equipment_activity_id}', headers=headers)
+        layers_data = layers_response.json()
+        print(json.dumps(layers_data, indent=2))
+
+        layer_id = layers_data['result'][0]['id']
+
+        # Step 3: GET - Get the list of Samples on a Layer
+        samples_response = requests.get(f'https://cloud.api.trimble.com/Trimble-Ag-Software/externalApi/3.0/tasks/{org_id}/layers/{layer_id}/samples', headers=headers)
+        samples_data = samples_response.json()
+        print(json.dumps(samples_data, indent=2))
+
     except Exception as error:
-        print(f"Error in Step 1 - GET Equipment Activities: {error}")
-        if activities_response:
+        print(f"Error in Steps: {error}")
+        if 'activities_response' in locals():
             print(f"Response data: {json.dumps(activities_response.json())}")
 
 if __name__ == "__main__":
     main()
+
